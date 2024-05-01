c61c647e276fdc4ddb402f9b3fd7936b0348aac1
Mentalflow <312902918@qq.com>
2024-05-02 00:20:36 +0800

    main: Add RibbonTemplateVersion to QML engine.

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,10 @@
 #ifdef RIBBONUI_BUILD_STATIC_LIB
 Q_IMPORT_QML_PLUGIN(RibbonUIPlugin)
 #endif
+
+#define STR(x) #x
+#define JOIN(a,b,c) STR(a.b.c)
+#define VER_JOIN(x) JOIN(x)
 int main(int argc, char *argv[])
 {
     RibbonUI::init(); // Must set before QGuiApplication
@@ -14,6 +18,7 @@ int main(int argc, char *argv[])
     RibbonUI::registerTypes(&engine);
     const QUrl url("qrc:/qt/qml/"+QString(RIBBONTEMPLATE_URI)+"/qml/template.qml"); // Edit your main page here
     engine.rootContext()->setContextProperty("RibbonTemplateUri", QString(RIBBONTEMPLATE_URI)); // Edit your custom app uri here
+    engine.rootContext()->setContextProperty("RibbonTemplateVersion", VER_JOIN(RIBBONTEMPLATE_VERSION)); // Edit your app version here
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
